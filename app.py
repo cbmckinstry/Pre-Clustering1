aaaa79101f1c7afbe6018fc5aeefdbf34466751c
carsonmckinstry <carson.mckinstry2021@gmail.com>
2025-12-16 04:32:02 -0600

    delete to 5s

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ ADMIN_TTL_SECONDS = int(os.environ.get("ADMIN_TTL_SECONDS", "60"))
 
 # If 0 => requires delete password every delete
 # If >0 => after entering delete password once, it stays unlocked for that many seconds
-DELETE_TTL_SECONDS = int(os.environ.get("DELETE_TTL_SECONDS", "60"))
+DELETE_TTL_SECONDS = int(os.environ.get("DELETE_TTL_SECONDS", "5"))
 
 def _now() -> float:
     return time.time()
